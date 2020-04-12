539025d2d0fc643ffb0f5e7a097bcc41c3ff4dab
František Gič <frantisek.gic@gmail.com>
2020-04-12 12:15:42 +0200

    Implement resizing and rehashing the hashTable

--- src/hashmap/quadratic-probing/quadratic-probing.c
+++ src/hashmap/quadratic-probing/quadratic-probing.c
@@ -7,6 +7,7 @@
 
 #define hash(table, key) (key%table->size)
 #define load_factor(keys, size) (keys / (size * 1.0))
+#define MAX_LOAD_FACTOR 0.5
 
 typedef struct HashTable {
     int keys;
@@ -14,20 +15,41 @@ typedef struct HashTable {
     int *table;
 } HashTable;
 
+void insert (HashTable **hashTable, int key);
+
+/**
+ * Creates and allocates the memory for a new hashtable
+ *
+ * @param size The size of the new hashtable
+ * @return Pointer to a newly created hashtable
+ */
 HashTable *new (int size) {
     HashTable *temp = (HashTable *) malloc(sizeof(HashTable));
     temp->keys = 0;
     temp->size = size;
-    temp->table = (int *) malloc(size * sizeof(int));
+    temp->table = (int *) calloc(size, sizeof(int));
     return temp;
 }
 
-//TODO: rehash
-HashTable *rehash (HashTable *table) {
-    return NULL;
+/**
+ * Create a new hashtable with double the size
+ * and rehash the old keys into the new one
+ *
+ * @param table The table to be resized and rehashed
+ * @return Pointer to a new allocated hashTable
+ */
+HashTable *rehash (HashTable *hashTable) {
+    HashTable *temp = new(hashTable->size * 2);
+    
+    for (int i = 0; i < hashTable->size; i++)
+        if (hashTable->table[i] != 0)
+            insert(&temp, hashTable->table[i]);
+    
+    free(hashTable);
+    return temp;
 }
 
-/*
+/**
  * Algorithm of quadratic probing inspired by
  * explanation of hashing algorithms by:
  * https://www.youtube.com/watch?v=mFY0J5W8Udk
@@ -48,10 +70,12 @@ int probe (HashTable *hashTable, int key) {
  */
 void insert (HashTable **hashTable, int key) {
     
-    /* If load factor of hashtable would be greater than 0.5 after insertion
+    /*
+     * If load factor of hashtable would be greater than 0.5 after insertion
      * rehash the table first
      */
-    if (load_factor(((*hashTable)->keys + 1), (*hashTable)->size) > 0.5)
+    float lf = load_factor(((*hashTable)->keys + 1), (*hashTable)->size);
+    if (lf > MAX_LOAD_FACTOR)
         *hashTable = rehash(*hashTable);
     
     // Find the suitable position in the table
@@ -76,32 +100,32 @@ int search (HashTable *hashTable, int value) {
     int i = 0;
     
     while (hashTable->table[(index + i * i) % hashTable->size] != value) {
+        // If the place is empty , it means the element does not exist in the hashtable
         if (hashTable->table[(index + i * i) % hashTable->size] == 0)
             return 0;
-        i++;
+        else
+            // Keep searching by increasing the i
+            i++;
     }
     
     return (index + i * i) % hashTable->size;
 }
 
-void printTable (HashTable *table) {
-    for (int i = 0; i < table->size; i++)
-        printf("[%d] %d\n", i, table->table[i]);
-}
-
 int main () {
-    
     HashTable *hashTable = new(10);
     
     insert(&hashTable, 76);
+    insert(&hashTable, 56);
     insert(&hashTable, 93);
     insert(&hashTable, 40);
     insert(&hashTable, 35);
     insert(&hashTable, 47);
-    
-    printTable(hashTable);
+    insert(&hashTable, 57);
+    insert(&hashTable, 23);
     
     int find = 47;
+    printf("Key of %d found at %d\n", find, search(hashTable, find));
+    find = 23;
     printf("Key of %d found at %d\n", find, search(hashTable, find));
     
     return 0;
